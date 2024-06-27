f50b1898da0e93c90d492d1c864e78c0386bce15
Tam Nguyen <tam@makini.io>
2024-06-27 10:26:42 +0700

    Change docs

---
+++ download_oa.php
@@ -0,0 +1,6 @@
+<?php
+
+file_put_contents('oas3_eam.json', file_get_contents('http://makini-api.test/request-docs/api?openapi=true&tag=EAM/CMMS'));
+file_put_contents('oas3_erp.json', file_get_contents('http://makini-api.test/request-docs/api?openapi=true&tag=ERP'));
+file_put_contents('oas3_wms.json', file_get_contents('http://makini-api.test/request-docs/api?openapi=true&tag=WMS'));
+file_put_contents('oas3_system.json', file_get_contents('http://makini-api.test/request-docs/api?openapi=true&tag=System'));
--- download_oa.sh
+++ download_oa.sh
@@ -1,4 +1,7 @@
-curl --location http://makini-api.test/request-docs/api\?openapi\=true\&tag\=EAM/CMMS -o oas3_eam.json
-curl --location http://makini-api.test/request-docs/api\?openapi\=true\&tag\=ERP -o oas3_erp.json
-curl --location http://makini-api.test/request-docs/api\?openapi\=true\&tag\=WMS -o oas3_wms.json
-curl --location http://makini-api.test/request-docs/api\?openapi\=true\&tag\=System -o oas3_system.json
+curl --location "http://makini-api.test/request-docs/api?openapi=true&tag=EAM/CMMS" -o oas3_eam.json
+curl --location http://makini-api.test/request-docs/api?openapi=true&tag=ERP -o oas3_erp.json
+curl --location http://makini-api.test/request-docs/api?openapi=true&tag=WMS -o oas3_wms.json
+curl --location http://makini-api.test/request-docs/api?openapi=true&tag=System -o oas3_system.json
+
+
+wget "http://makini-api.test/request-docs/api?openapi=true&tag=EAM/CMMS" -O oas3_eam.json
